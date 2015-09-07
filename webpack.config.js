f1348f352e1d115400b595c38fe815fa7499d4f6
Sambya Aryasa <sbycrosz@gmail.com>
2015-09-07 21:00:57 +0800

    move sources to its own directory

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
+  context: path.join(__dirname, '/src'),
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
@@ -25,19 +26,3 @@ module.exports = {
     }]
   }
 };
-
-// When inside Redux repo, prefer src to compiled version.
-// You can safely delete these lines in your project.
-var reduxSrc = path.join(__dirname, '..', '..', 'src');
-var reduxNodeModules = path.join(__dirname, '..', '..', 'node_modules');
-var fs = require('fs');
-if (fs.existsSync(reduxSrc) && fs.existsSync(reduxNodeModules)) {
-  // Resolve Redux to source
-  module.exports.resolve = { alias: { 'redux': reduxSrc } };
-  // Compile Redux from source
-  module.exports.module.loaders.push({
-    test: /\.js$/,
-    loaders: ['babel'],
-    include: reduxSrc
-  });
-}
