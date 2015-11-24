0a2a80951a7d115a8494fe6feeda55bb2f71d5e1
Sambya Aryasa <sbycrosz@gmail.com>
2015-11-24 17:22:06 +0800

    refactor dumb components

--- src/components/FoodDiary/DiaryHeader.js
+++ src/components/FoodDiary/DiaryHeader.js
@@ -1,28 +1,18 @@
 import React, { Component, PropTypes } from 'react';
 import { formatDate } from '../../utilities/DateUtilities'
 
-export default class DiaryHeader extends Component {
-  static propTypes = {
-    displayedDate: PropTypes.instanceOf(Date).isRequired,
-    gotoPreviousDay: PropTypes.func.isRequired,
-    gotoNextDay: PropTypes.func.isRequired
-  }
+const DiaryHeader = (props) => {
+  return (<div className='diary-header'>
+            <a className='previous-day'
+               title="Go to previous day"
+               onClick={props.gotoPreviousDay}/>
+            <h2>
+              { formatDate(props.displayedDate) }
+            </h2>
+            <a className='next-day'
+               title="Go to next day"
+               onClick={props.gotoNextDay}/>
+          </div>);
+};
 
-  render() {
-    const { displayedDate, gotoPreviousDay, gotoNextDay } = this.props;
-
-    return (
-      <div className='diary-header'>
-        <a className='previous-day'
-           title="Go to previous day"
-           onClick={gotoPreviousDay}/>
-        <h2>
-          { formatDate(displayedDate) }
-        </h2>
-        <a className='next-day'
-           title="Go to next day"
-           onClick={gotoNextDay}/>
-      </div>
-    );
-  }
-}
+export default DiaryHeader;
--- src/components/FoodDiary/DiarySection.js
+++ src/components/FoodDiary/DiarySection.js
@@ -1,19 +1,14 @@
 import React, { Component, PropTypes } from 'react';
 import DiaryEntry from './DiaryEntry';
 
-export default class DiarySection extends Component {
-  static propTypes = {
-    name: PropTypes.string.isRequired,
-    entries: PropTypes.array.isRequired
-  }
-
-  render(){
-    const { name, entries } = this.props;
-    return (
-      <div className={"diary-section " + name.toLowerCase() }>
-        <h3 className="section-header"> { name } </h3>
-        { entries.map((entry) => <DiaryEntry key={entry.id} {...entry}/>)}
+const DiarySection = (props) => {
+  return (
+      <div className={"diary-section " + props.name.toLowerCase() }>
+        <h3 className="section-header"> { props.name } </h3>
+        { props.entries.map((entry) =>
+            <DiaryEntry key={entry.id} {...entry}/>)}
       </div>
     );
-  }
-}
+};
+
+export default DiarySection;
